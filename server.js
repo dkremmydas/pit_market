50ceb8fb7bbc13e822e1aaf36bf9d57a2d51ffe9
DimKrem <kremmydas@aua.gr>
2018-10-04 00:49:01 +0300

    Update server.js - Codepanel

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (request, response) => {
-  response.send('Hello from Express!')
-})
+app.get('/sell_bid', (request, response) => {
+  response.send('Hello from Express!');
+});
+
+app.get('/buy_bid', (request, response) => {
+  response.send('Hello from Express!');
+});
+
+
+
 
 app.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.log('something bad happened', err);
   }
 
-  console.log(`server is listening on ${port}`)
-})
+  console.log(`server is listening on ${port}`);
+});
