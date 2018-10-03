c134c206cf38d609a056959593be01769cfe4ae5
Dimitris Kremmydas <kremmydas@aua.gr>
2018-10-04 01:26:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,38 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/sell_bid', (request, response) => {
-  response.send('Hello from Express!');
+
+//npm install lokijs
+//npm install express
+
+var db = new loki('loki.json')
+
+var bids = db.addCollection('bids')
+var transactions = db.addCollection('transactions')
+
+
+app.get('/sell_bid/:seller/:price', (request, response) => {
+  bids.insert({'seller':request.params.seller, 'price': request.params.price});
+  console.log("Sell bid: "+request.params.seller + " @ " + request.params.price);
+  response.json({'reply': "ok"});
 });
 
-app.get('/buy_bid', (request, response) => {
-  response.send('Hello from Express!');
+app.get('/buy_bid/:buyer/:seller/:price', (request, response) => {
+  var trans = bids.findOne( {'seller':request.params.seller, 'price': request.params.price} ).remove();
+  if(!trans===null) {
+    transactions.insert({'buyer': request.params.buyer,'seller':request.params.seller, 'price': request.params.price});
+    response.json({'reply': "ok"});
+  } else {
+    response.json({'reply': "no bid like this"});
+  }  
+});
+
+app.get('/bids', (request, response) => {
+  response.json(bids.data());
+});
+
+app.get('/transactions', (request, response) => {
+  response.json(transactions.data());
 });
 
 
